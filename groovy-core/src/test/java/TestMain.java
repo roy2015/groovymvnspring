09f53f62e1df02a02436eb076212dfeeb54751e6
roy2015 <roy2010@163.com>
2017-01-11 16:34:31 +0800

    test case 修改

--- groovy-core/src/test/java/TestMain.java
+++ groovy-core/src/test/java/TestMain.java
@@ -2,6 +2,9 @@
  * Created by BG244210 on 1/11/2017.
  */
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 /**
  * 新建java test 20170111 15:35
  */
@@ -11,7 +14,12 @@ public class TestMain {
      * 丢失精度
      */
     public static void test1(){
-        System.out.println(18.2 + 0.18 *30);
+        double k = 18.2 + 0.18 *30;
+        BigDecimal bd = new BigDecimal(k);
+        bd = bd.setScale(2, RoundingMode.HALF_UP);
+        double d = bd.doubleValue();
+        System.out.println(k);
+        System.out.println(d);
     }
 
     public static void main(String[] args) {
