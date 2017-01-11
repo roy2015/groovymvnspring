b3b2dd908cee920d59ef6b84cc60f2679c4f120b
roy2015 <roy2010@163.com>
2017-01-11 15:39:28 +0800

    add java testcase without framework

---
+++ groovy-core/src/test/java/TestMain.java
@@ -0,0 +1,20 @@
+/**
+ * Created by BG244210 on 1/11/2017.
+ */
+
+/**
+ * 新建java test 20170111 15:35
+ */
+public class TestMain {
+
+    /**
+     * 丢失精度
+     */
+    public static void test1(){
+        System.out.println(18.2 + 0.18 *30);
+    }
+
+    public static void main(String[] args) {
+        test1();
+    }
+}
